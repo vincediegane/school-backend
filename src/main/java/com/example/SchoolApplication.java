74f24cc030b3457a318d81df00e574da6c1919c3
vincent <vincent.faye@agilicis.com>
2024-11-14 11:06:02 +0000

    refacoting code

--- src/main/java/com/example/SchoolApplication.java
+++ src/main/java/com/example/SchoolApplication.java
@@ -23,32 +23,32 @@ public class SchoolApplication {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
 
-//	@Bean
-//	CommandLineRunner run(StudentRepository studentRepository, PaymentRepository paymentRepository) {
-//		return args -> {
-//			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Vincent").lastName("Faye").code("112233").programId("PCSM").build());
-//			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Babacar").lastName("Kadame").code("112244").programId("MPI").build());
-//			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Rokhaya").lastName("Diouf").code("112255").programId("BCG").build());
-//			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Aissatou").lastName("Kante").code("112266").programId("PCSM").build());
-//			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Modou").lastName("Diao").code("112277").programId("MPI").build());
-//
-//			PaymentType[] paymentTypes = PaymentType.values();
-//			Random random = new Random();
-//			List<Student> students = studentRepository.findAll();
-//
-//			students.forEach(student -> {
-//				for(int i = 0; i < students.size(); i++) {
-//					int index = random.nextInt(paymentTypes.length);
-//					Payment payment = Payment.builder()
-//						.amount(1000+(int)(Math.random()+20000))
-//						.type(paymentTypes[index])
-//						.status(PaymentStatus.CREATED)
-//						.date(LocalDate.now())
-//						.student(student)
-//						.build();
-//					paymentRepository.save(payment);
-//				}
-//			});
-//		};
-//	}
+	@Bean
+	CommandLineRunner run(StudentRepository studentRepository, PaymentRepository paymentRepository) {
+		return args -> {
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Vincent").lastName("Faye").code("112233").programId("PCSM").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Babacar").lastName("Kadame").code("112244").programId("MPI").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Rokhaya").lastName("Diouf").code("112255").programId("BCG").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Aissatou").lastName("Kante").code("112266").programId("PCSM").build());
+			studentRepository.save(Student.builder().id(UUID.randomUUID().toString()).firstName("Modou").lastName("Diao").code("112277").programId("MPI").build());
+
+			PaymentType[] paymentTypes = PaymentType.values();
+			Random random = new Random();
+			List<Student> students = studentRepository.findAll();
+
+			students.forEach(student -> {
+				for(int i = 0; i < students.size(); i++) {
+					int index = random.nextInt(paymentTypes.length);
+					Payment payment = Payment.builder()
+						.amount(1000+(int)(Math.random()+20000))
+						.type(paymentTypes[index])
+						.status(PaymentStatus.CREATED)
+						.date(LocalDate.now())
+						.student(student)
+						.build();
+					paymentRepository.save(payment);
+				}
+			});
+		};
+	}
 }
